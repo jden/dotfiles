3ce44de1f853c57c2ddcfa21ee7bee1eb73cb4da
jden <jden@zendesk.com>
2016-08-05 16:46:08 -0700

    save settings

--- install.sh
+++ install.sh
@@ -2,5 +2,12 @@
 set -x
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/scripts/git-uncommit.sh ~/bin/git-uncommit
+ln -sf ~/.dotfiles/scripts/git-thank.sh ~/bin/git-thank
 set +x
+echo installing some other tools
+
+# https://github.com/paulirish/git-recent/
+npm install --global git-recent
+
+
 echo setup ok
