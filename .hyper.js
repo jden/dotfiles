fdbf952735f31fa090dc5083bcb2aa8650725c23
jsdnxx <jason@suarez.email>
2016-12-08 14:02:27 -0800

    save settings

--- .hyper.js
+++ .hyper.js
@@ -81,7 +81,11 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    'hyperlinks-iterm',
+    'hypercwd',
+    'hyperterm-subpixel-antialiased'
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
