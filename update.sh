e2730114bae5ccea319ac4709d3e0466e2e1560b
Juno Suárez <juno@scroll.com>
2019-05-14 14:01:16 -0700

    Add stubby

--- update.sh
+++ update.sh
@@ -7,9 +7,15 @@ packages=(
   inetutils
   reattach-to-user-namespace # see see https://github.com/Microsoft/vscode/issues/12587#issuecomment-280681178
   cloc
+  stubby # secure dns
 )
 brew install ${packages[*]}
 brew upgrade
+
+# setup stubby
+sudo brew services start stubby
+sudo /usr/local/opt/stubby/sbin/stubby-setdns-macos.sh
+ln -sf ~/.dotfiles/files/stubby.yml /usr/local/etc/stubby/stubby.yml
 
 casks=(
   hyper
