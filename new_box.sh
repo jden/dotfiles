571f7f42c249d7388829c63a0822d916ca20fa02
junosuarez <juno@suarez.email>
2019-09-04 22:53:00 -0700

    save settings

--- new_box.sh
+++ new_box.sh
@@ -6,5 +6,7 @@ if needs brew; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
 ./install.sh
 ./update.sh
