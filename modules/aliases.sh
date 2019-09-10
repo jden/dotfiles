1a3d536413d28a3ec82f66ddee5cd694d07d7f31
Juno Suárez <juno@scroll.com>
2019-09-10 10:55:51 -0700

    no quotes on syncrc

--- modules/aliases.sh
+++ modules/aliases.sh
@@ -28,14 +28,14 @@ alias editrc="edit $DOTFILES"
 alias gitrc="git --git-dir=$DOTFILES/.git --work-tree=$DOTFILES"
 alias pullrc="gitrc pull origin master"
 function commitrc () {
-  message=${1:-save settings}
+  message="${1:-save settings}"
   gitrc commit -am "$message" || return 1
   log_shell_event dotfiles.saved
 }
 alias pushrc="gitrc push origin master"
 function syncrc () {
   pullrc || return 1
-  commitrc "$@" || return 1
+  commitrc "$*" || return 1
   pushrc || return 1
 }
 alias glorc="gitrc log --pretty=format:'%C(dim white)%h%Creset %C(bold white)%>(15)%ar%Creset %Cgreen%d%Creset %s' --color=always"
