f7f3e75d20b1cde453823fc3122300d60368f2c5
junosuarez <juno@suarez.email>
2020-10-24 20:57:28 -0700

    tex support

--- modules/aliases.sh
+++ modules/aliases.sh
@@ -57,6 +57,10 @@ alias isvg2="rsvg-convert --zoom=2 | icat"
 alias ivl="vl2svg | isvg2"
 ## dot with default styles, reference https://graphviz.org/doc/info/attrs.html
 alias idot="dot -Gbgcolor=transparent -Ncolor=white -Nfontcolor=white -Nfontsize=16 -Nfontname=monospace -Ecolor=white -Tsvg | isvg; echo"
+function itex() {
+  tex2svg "$1" | sed 's|</svg>|<style>*{fill:white;}</style></svg>|' | rsvg-convert --zoom 3 | icat
+}
+
 
 # git aliases
 alias g=git
