6759bce9e50a1a4e66f42176dbf0d7d987e46fde
juno <juno@localhost>
2022-09-01 01:43:31 -0700

    save settings

--- graph.zsh
+++ graph.zsh
@@ -1,32 +1,51 @@
 #!/bin/zsh
 source ~/.dotfiles/mod.zsh
 
-function __initModule() {
-  local module=$1
-  local modpath=$DOTFILES/modules/$1
-    echo $module
+declare -a lines=()
 
-  if [[ -f $modpath/info ]]; then
-    source $modpath/info
-  else
-    echo "  (no deps)"
-    echo
+function __on_mod_cb() {
+  local name=${MOD_CURRENT[name]}
+  if [[ ${#MOD_CURRENT[use]} -gt 0 ]]; then
+    lines+="  \"$name\" -> {"
+    for d in ${(z)MOD_CURRENT[use]}; do
+      lines+="    \"$d\""
+    done
+    lines+="  }"
   fi
-
-  # echo p $_pending
-  head=(${_pending:0:1})
-  tail=(${_pending:1})
-  # echo h $head
-  # echo t $tail
-  _pending=(${_pending:1})
-  if [[ "$head" != "" ]]; then
-  echo
-  __initModule $head
+  if [[ ${#MOD_CURRENT[brew]} -gt 0 ]]; then
+    lines+="  \"$name\" -> {"
+    for d in ${(z)MOD_CURRENT[brew]}; do
+      lines+="    \"b_$d\" [shape=box]"
+    done
+    lines+="  }"
   fi
-
-  # TODO: break cycles
 }
 
-__initModule $1
-echo
-echo brew: $MOD_BREW
+lines+="digraph A {"
+__walkModules main_profile "__on_mod_cb"
+
+if [[ ${#MOD_BREWS} -gt 0 ]]; then
+  lines+="  subgraph cluster_BREW { rank=same;"
+  for d in ${(z)MOD_BREWS}; do
+    lines+="    \"b_$d\" [shape=box, label=\"$d\"];"
+  done
+  lines+="    label = \"brew\";"
+  lines+="  }"
+fi
+
+  lines+="  subgraph cluster_legend { color=white; shape=note; "
+  lines+="    node [shape=box, label=\"brew\"]l1;"
+  lines+="    node [shape=oval, label=\"module\"]l2;"
+  # lines+="    label = \"key\";"
+  lines+="  }"
+
+lines+="}"
+
+out="${(j:\n:)lines}"
+
+if [[ "$@" =~ "-raw" ]]; then
+  echo $out
+else
+  source $DOTFILES/modules/kitty/alias.zsh
+  echo $out | idot
+fi
