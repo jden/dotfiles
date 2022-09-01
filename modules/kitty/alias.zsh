d82b20c30fad9faeefe25e8daafca1c97f5c2d87
juno suárez <jsuarez@twitter.com>
2022-09-01 16:12:41 -0700

    fix graph imagemagick

--- modules/kitty/alias.zsh
+++ modules/kitty/alias.zsh
@@ -6,8 +6,15 @@ alias isvg2="rsvg-convert --zoom=2 | icat"
 alias ivl="vl2svg | isvg2"
 ## dot with default styles, reference https://graphviz.org/doc/info/attrs.html
 function idot() {
+  cleanpath
   dot -Gbgcolor=transparent -Ncolor=white -Nfontcolor=white -Nfontsize=16 -Nfontname=monospace -Ecolor=white -Tsvg | isvg
 }
 function itex() {
   tex2svg "$1" | sed 's|</svg>|<style>*{fill:white;}</style></svg>|' | rsvg-convert --zoom 3 | icat
+}
+
+function cleanpath() {
+  local split=(${(s|:|)PATH})
+  local clean=${(j|:|)${split:#*mde*}}
+  PATH=$clean
 }
