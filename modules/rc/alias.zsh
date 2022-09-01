b560e5d894f4850f71eea0abd2d39f706ebbc560
juno suárez <jsuarez@twitter.com>
2022-09-01 11:55:02 -0700

    fix spacing

--- modules/rc/alias.zsh
+++ modules/rc/alias.zsh
@@ -46,10 +46,10 @@ function rc-sync () {
   # TODO: this doesnt work, need to make an inner rc-source # ensure we have the latest
   local untracked=$(gitrc status --porcelain=1 | grep -e '^??')
   if [[ $untracked != "" ]]; then
-  gitrc status
-  echo
-  echo "use 'gitrc add -A' if you want to continue"
-  return 1
+    gitrc status
+    echo
+    echo "use 'gitrc add -A' if you want to continue"
+    return 1
   fi
 
   local om1=$(git rev-parse origin/master)
