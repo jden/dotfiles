1fc2086e9e99ddfe5cd69356c566e84aaf005ed8
juno <juno@localhost>
2022-09-10 20:05:38 -0700

    add Spoons

--- modules/hotkeys/hammerspoon.lua
+++ modules/hotkeys/hammerspoon.lua
@@ -1,6 +1,14 @@
 -- see https://www.hammerspoon.org/docs/hs.hotkey.html
 
-hs.hotkey.bind("ctrl+option+shift", "z", function()
-  -- use with karabiner mod
+-- hs.hotkey.bind("ctrl+option+shift", "z", function()
+--   -- use with karabiner mod
+--   hs.application.launchOrFocus("kitty")
+-- end)
+
+hs.loadSpoon("ReloadConfiguration")
+spoon.ReloadConfiguration:start()
+
+hs.hotkey.bind("ctrl+option+cmd", "right", function()
+  -- MoveToNextDisplay
   hs.application.launchOrFocus("kitty")
 end)
--- modules/hotkeys/init.zsh
+++ modules/hotkeys/init.zsh
@@ -5,3 +5,7 @@ ln -sf $DOTFILES/modules/hotkeys/remap.json ~/.config/karabiner/assets/complex_m
 # hammerspoon
 mkdir -p ~/.hammerspoon
 ln -sf $DOTFILES/modules/hotkeys/hammerspoon.lua ~/.hammerspoon/init.lua
+
+mkdir -p ~/.hammerspoon/Spoons
+find $DOTFILES/modules/hotkeys/@Hammerspoon/Spoons/Spoons/*.spoon.zip \
+  | xargs -n1 -I{} sh -c 'unzip -oq {} -d ~/.hammerspoon/Spoons'
