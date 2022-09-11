228464c800b79316dc41455329fe714e01086a49
juno <juno@localhost>
2022-09-11 00:00:41 -0700

    clean spectacle

--- modules/window_mgr/init.zsh
+++
@@ -1,4 +0,0 @@
-# control spectacle settings
-# mkdir -p "$HOME/Library/Application Support/Spectacle"
-# rm -f "$HOME/Library/Application Support/Spectacle/Shortcuts.json"
-# ln -sf $DOTFILES/modules/window_mgr/spectacle.json "$HOME/Library/Application Support/Spectacle/Shortcuts.json"
