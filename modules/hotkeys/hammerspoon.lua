24832e4248a585d1f9a3d3b04914e92b55088abb
juno <juno@localhost>
2022-09-10 21:04:13 -0700

    replace spectacle with hammerspoon

--- modules/hotkeys/hammerspoon.lua
+++ modules/hotkeys/hammerspoon.lua
@@ -8,7 +8,46 @@
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
+hs.loadSpoon("WindowHalfsAndThirds")
+hs.loadSpoon("WindowScreenLeftAndRight")
+spoon.WindowScreenLeftAndRight:bindHotkeys(spoon.WindowScreenLeftAndRight.defaultHotkeys)
+
+hs.window.animationDuration = 0
+
 hs.hotkey.bind("ctrl+option+cmd", "right", function()
   -- MoveToNextDisplay
   hs.application.launchOrFocus("kitty")
 end)
+
+hs.hotkey.bind("ctrl+cmd", "right", function()
+  -- MoveToRightHalf
+  local win = hs.window.frontmostWindow()
+  spoon.WindowHalfsAndThirds:rightHalf(win)
+end)
+
+hs.hotkey.bind("ctrl+cmd", "left", function()
+  -- MoveToLeftHalf
+  -- hs.alert.show("l")
+  local win = hs.window.frontmostWindow()
+  spoon.WindowHalfsAndThirds:leftHalf(win)
+end)
+
+hs.hotkey.bind("ctrl+cmd", "up", function()
+  -- MoveToFullscreen
+  local win = hs.window.frontmostWindow()
+  win:move(hs.layout.maximized)
+end)
+
+-- as fate would have it, these are the default
+-- hot keys for spoon.WindowScreenLeftAndRight:bindHotkeys(spoon.WindowScreenLeftAndRight.defaultHotkeys)
+-- hs.hotkey.bind("ctrl+option+cmd", "left", function()
+--   -- MoveToPreviousDisplay
+--   local win = hs.window.frontmostWindow()
+--   win:move(hs.layout.maximized)
+-- end)
+
+-- hs.hotkey.bind("ctrl+option+cmd", "right", function()
+--   -- MoveToNextDisplay
+--   local win = hs.window.frontmostWindow()
+
+-- end)
