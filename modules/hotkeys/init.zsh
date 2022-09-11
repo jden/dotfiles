6f7fee518b8b792624c45ca87dee5a2e1a6a8b3b
juno <juno@localhost>
2022-09-10 22:26:05 -0700

    fix hotkey init

--- modules/hotkeys/init.zsh
+++ modules/hotkeys/init.zsh
@@ -5,6 +5,7 @@ ln -sf $DOTFILES/modules/hotkeys/remap.json ~/.config/karabiner/assets/complex_m
 # hammerspoon
 mkdir -p ~/.hammerspoon
 ln -sf $DOTFILES/modules/hotkeys/hammerspoon.lua ~/.hammerspoon/init.lua
+rm -f ~/.hammerspoon/Spoons
 ln -sf $DOTFILES/modules/hotkeys/@Hammerspoon/Spoons/Source ~/.hammerspoon/Spoons
 
 ln -sf /Applications/Hammerspoon.app/Contents/Frameworks/hs/hs ~/bin/hs
