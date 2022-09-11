61c47e48e8536770323be4733574a5e02f0dbd58
juno <juno@localhost>
2022-09-10 21:05:42 -0700

    rm spectacle

--- modules/window_mgr/init.zsh
+++ modules/window_mgr/init.zsh
@@ -1,4 +1,4 @@
 # control spectacle settings
-mkdir -p "$HOME/Library/Application Support/Spectacle"
-rm -f "$HOME/Library/Application Support/Spectacle/Shortcuts.json"
-ln -sf $DOTFILES/modules/window_mgr/spectacle.json "$HOME/Library/Application Support/Spectacle/Shortcuts.json"
+# mkdir -p "$HOME/Library/Application Support/Spectacle"
+# rm -f "$HOME/Library/Application Support/Spectacle/Shortcuts.json"
+# ln -sf $DOTFILES/modules/window_mgr/spectacle.json "$HOME/Library/Application Support/Spectacle/Shortcuts.json"
